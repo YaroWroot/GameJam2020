03052ce59c69370d8bb73890ef263c7773621a7c
Alexander <protonull@protonmail.com>
2020-03-01 12:19:30 +0000

    1123

--- Assets/Scripts/Inventory/Weapons/Sword.cs
+++ Assets/Scripts/Inventory/Weapons/Sword.cs
@@ -7,11 +7,17 @@ public class Sword : Weapon
 
     void Awake()
     {
-        _range = 1.5f;
+        _range = 2.5f;
         _equippedTransform.position.Set(0.021f, 0f, 0.0253f);
         _equippedTransform.rotation.Set(90f, 0f, 90f);
         _sheathedTransform.position.Set(-0.1f, 0.19f, 0f);
         _sheathedTransform.rotation.Set(155.43f, 90f, 0f);
     }
-    
+
+    public override bool Use(WeaponUser player, Health against)
+    {
+        against.TakeDamage(3);
+        return true;
+    }
+
 }
